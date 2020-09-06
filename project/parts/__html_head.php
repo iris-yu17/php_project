933a545695d9813d3960706e2b9fffab51e2c2ea
Iris Yu <iris.yu0716@hmail.com>
2020-09-06 22:43:10 +0800

    add

---
+++ project/parts/__html_head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=WEB_ROOT?>/bootstrap/css/bootstrap.css">
+
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/29bbea6937.js" crossorigin="anonymous"></script>
+
+    <!-- 加$page_title -->
+    <title><?= $page_title ?? '' ?> 通訊錄</title>
+
+</head>
+
+<body>
